3e08a5ea6a0c5373d30de2ed796e1e0e6abe70f1
John Li <m_listbox@163.com>
2026-09-05 11:43:53 +0800

    job

--- main/main.go
+++ main/main.go
@@ -4,19 +4,22 @@ import (
 	"bufio"
 	"container/list"
 	"fmt"
-	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
-	"unicode/utf8"
+)
+
+var (
+	re, re2, re3, re4 *regexp.Regexp
 )
 
 type Question struct {
-	Id     int                    `json:"Id"`
-	Head   string                 `json:"Head"`
-	Answer string                 `json:"Answer"`
-	Class  string                 `json:"Class"`
-	Option map[string]interface{} `json:"Option"`
+	Id     int               `json:"Id"`
+	Head   string            `json:"Head"`
+	Answer string            `json:"Answer"`
+	Class  string            `json:"Class"`
+	Option map[string]string `json:"Option"`
 }
 
 func NewQuestion(id int, head string, answer string, class string) *Question {
@@ -25,11 +28,15 @@ func NewQuestion(id int, head string, answer string, class string) *Question {
 		Head:   head,
 		Answer: answer,
 		Class:  class,
-		Option: make(map[string]interface{}),
+		Option: make(map[string]string),
 	}
 }
 func (q *Question) ToString() string {
-	return fmt.Sprintf("questionID : %d ; Answer: %s class: %s ;Head: %s ;Option: %v\n", q.Id, q.Answer, q.Class, q.Head, q.Option)
+	o := ""
+	for k, v := range q.Option {
+		o = o + fmt.Sprintf("%s:%s\n", k, v)
+	}
+	return fmt.Sprintf("%d.%s;[%s](%s)\n %s", q.Id, q.Head, q.Class, q.Answer, o)
 }
 
 func getFirstCharRange(s string) string {
@@ -39,11 +46,12 @@ func getFirstCharRange(s string) string {
 	return ""
 }
 func StringToMap(q *Question, it string, re *regexp.Regexp) {
-	strs := strings.Split(it, "；")
+	//strs := strings.Split(it, " ")
+	strs := re.FindAllString(it, -1)
 	for _, str := range strs {
 		if re.MatchString(str) {
 			//s := re.FindString(str)
-			sl := strings.Split(str, "、")
+			sl := strings.Split(str, ".")
 			q.Option[getFirstCharRange(sl[0])] = sl[1]
 
 		}
@@ -51,29 +59,59 @@ func StringToMap(q *Question, it string, re *regexp.Regexp) {
 
 }
 func extractWithIndex(s string, st rune, et rune) string {
-	start := strings.IndexRune(s, st)
-	end := strings.IndexRune(s, et)
-
-	// 确保括号存在且顺序正确
-	if start == -1 || end == -1 || end <= start {
-		return ""
+	//r := re.FindAllString(s, -1)
+	start := strings.Split(s, string(st))
+	for _, it := range start {
+		if i := strings.Index(it, string(et)); i == 0 || i == -1 {
+			continue
+		} else {
+			return it[0:i]
+		}
 	}
+	return ""
 
-	return s[start+utf8.RuneLen(st) : end]
+}
+
+func getAnswerAndClass(s string, st rune, et rune) (string, string) {
+	c := strings.Count(s, ")")
+	for i := 0; i < c; i++ {
+		a := extractWithIndex(s, st, et)
+		a = strings.Trim(a, " ")
+		if strings.Contains(a, "对") || strings.Contains(a, "错") {
+			return a, "判断题'"
+		} else if re4.MatchString(a) && len(a) == 1 {
+			return a, "单选题"
+		} else if re4.MatchString(a) && len(a) > 1 {
+			return a, "多选题"
+		} else if a == "" {
+			s = strings.Replace(s, ")", "", 1)
+		}
+		s = strings.ReplaceAll(s, a, "")
+	}
+	return "", "未知"
+}
+func deleteredundant(s string, st string, et string) string {
+	if strings.Count(s, et) > 1 {
+		a, c := getAnswerAndClass(s, '(', ')')
+		if c != "未知" {
+			lstrs := strings.Split(s, ")")
+			s = ""
+			for _, lstr := range lstrs {
+				if strings.Contains(lstr, a) {
+					lstr = lstr + ")"
+				}
+				s = s + lstr
+			}
+			//line = strings.Replace(line, ")", "", strings.Count(line, ")")-1)
+
+		}
+
+	}
+	return s
 }
 
 // SearchFileByLine 逐行搜索文件，返回匹配的行号和內容
-func SearchFileByLine(ls *list.List, filename, pattern1 string, pattern2 string) error {
-	// 1. 预编译正则
-	re, err := regexp.Compile(pattern1)
-	if err != nil {
-		return fmt.Errorf("invalid regex1: %w", err)
-	}
-
-	re2, err := regexp.Compile(pattern2)
-	if err != nil {
-		return fmt.Errorf("invalid regex2: %w", err)
-	}
+func SearchFileByLine(ls *list.List, filename string) error {
 
 	// 2. 打开文件
 	file, err := os.Open(filename)
@@ -100,21 +138,27 @@ func SearchFileByLine(ls *list.List, filename, pattern1 string, pattern2 string)
 		// 4. 执行匹配
 		if re.MatchString(line) {
 			//indices := re.FindAllStringIndex(line, -1)
+
 			c := strings.Count(line, "]")
 			if strings.HasSuffix(line, "]") && c == 1 {
+				line = deleteredundant(line, "(", ")")
 				//indices := re.FindAllStringIndex(, -1)
 				s := re.FindString(line)
 				is := re.FindStringIndex(line)
+				//h := re.FindStringSubmatch(line)
 				//indices := re.FindAllStringIndex(s, -1)
 				//s := re.FindAllStringSubmatch(line, -1)
 				questionID++
 				//log.Printf("Line %d: questionID : %d . %s ; Answer: %s class: %s\n", lineNum, questionID, s[strings.Index(s, ".")+1:], extractWithIndex(s, '（', '）'), extractWithIndex(s, '[', ']'))
-
-				e = ls.PushBack(NewQuestion(questionID, s, extractWithIndex(s, '（', '）'), extractWithIndex(s, '[', ']')))
+				a, c := getAnswerAndClass(s, '(', ')')
+				e = ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.Index(s, "[")], a, c))
+				//e = ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.Index(s, "[")], extractWithIndex(s, '(', ')'), extractWithIndex(s, '[', ']')))
+				//log.Printf("%v ", e.Value.(*Question).ToString())
 
 				if len(s) < len(line) {
 					if ev, ok := e.Prev().Value.(*Question); ok {
 						StringToMap(ev, line[0:is[0]], re2)
+						//StringToMap(ev, line[is[1]:], re2)
 					}
 				}
 
@@ -123,18 +167,24 @@ func SearchFileByLine(ls *list.List, filename, pattern1 string, pattern2 string)
 				strs := strings.Split(line, "]")
 				for i := 0; i < c; i++ {
 					strs[i] = strs[i] + "]"
+					strs[i] = deleteredundant(strs[i], "(", ")")
 					s := re.FindString(strs[i])
 					is := re.FindStringIndex(strs[i])
 					//s := re.FindStringSubmatch(strs[i])
 					questionID++
-
-					//log.Printf("Line %d: questionID : %d . %s ; Answer: %s class: %s\n", lineNum, questionID, s[strings.Index(s, ".")+1:], extractWithIndex(s, '（', '）'), extractWithIndex(s, '[', ']'))
-					e = ls.PushBack(NewQuestion(questionID, s, extractWithIndex(s, '（', '）'), extractWithIndex(s, '[', ']')))
+					a, c := getAnswerAndClass(s, '(', ')')
+					e = ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.Index(s, "[")], a, c))
+					//log.Printf("Line %d: questionID : %d . %s ; Answer: %s class: %s\n", lineNum, questionID, s[strings.Index(s, ".")+1:], extractWithIndex(s, '(', ')'), extractWithIndex(s, '[', ']'))
+					//e = ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.Index(s, "[")], extractWithIndex(s, '(', ')'), extractWithIndex(s, '[', ']')))
+					//log.Printf("%v ", e.Value.(*Question).ToString())
 					if len(s) < len(strs[i]) {
 						if ev, ok := e.Prev().Value.(*Question); ok {
 							StringToMap(ev, strs[i][0:is[0]], re2)
 						}
 					}
+				}
+				if len(strs) > c {
+					StringToMap(e.Value.(*Question), strs[len(strs)-1], re2)
 				}
 			}
 
@@ -147,6 +197,32 @@ func SearchFileByLine(ls *list.List, filename, pattern1 string, pattern2 string)
 				StringToMap(ev, line, re2)
 			}
 		}
+		if re3.MatchString(line) && !strings.Contains(line, "]") {
+			line = deleteredundant(line, "(", ")")
+			strs := re3.FindAllString(line, -1)
+			for _, s := range strs {
+
+				/*
+					a := extractWithIndex(s, '(', ')')
+					if strings.Index(a, "对") != -1 || strings.Index(a, "错") != -1 {
+						questionID++
+						ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.LastIndex(s, "(")], a, "判断题"))
+					} else if re4.MatchString(a) && len(a) == 1 {
+						questionID++
+						ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.LastIndex(s, "(")], a, "单选题"))
+					} else if re4.MatchString(a) && len(a) > 1 {
+						questionID++
+						ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.LastIndex(s, "(")], a, "多选题"))
+					} else {
+						ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.LastIndex(s, "(")], a, "未知"))
+					}
+				*/
+				a, c := getAnswerAndClass(s, '(', ')')
+				questionID++
+				ls.PushBack(NewQuestion(questionID, s[strings.Index(s, ".")+1:strings.LastIndex(s, "(")], a, c))
+
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -156,14 +232,128 @@ func SearchFileByLine(ls *list.List, filename, pattern1 string, pattern2 string)
 	return nil
 }
 
+type KV struct {
+	Key   string
+	Value int
+}
+
+func sortMap(m map[string]int) (*[]KV, int) {
+
+	s := make([]KV, 0, len(m))
+	sum := 0
+	for k, v := range m {
+		s = append(s, KV{k, v})
+		sum = sum + v
+	}
+
+	sort.Slice(s, func(i, j int) bool {
+		return s[i].Value > s[j].Value
+	})
+	return &s, sum
+}
+
 func main() {
 
 	ls := list.New()
-	err := SearchFileByLine(ls, "Source.txt", `(\d+)\.(.*)`, `^[A-Z]、`)
+	// 1. 预编译正则
+	var err error
+	re, err = regexp.Compile(`\d+\..*\]`)
+	if err != nil {
+		fmt.Print("invalid regex1: %w", err)
+	}
+
+	re2, err = regexp.Compile(`[A-Z]\.[^ ]*`)
+	if err != nil {
+		fmt.Print("invalid regex2: %w", err)
+	}
+
+	re3, err = regexp.Compile(`\d+\.[^\]\)]*\)`)
+	if err != nil {
+		fmt.Print("invalid regex3: %w", err)
+	}
+
+	re4, err = regexp.Compile(`^[A-Z]`)
+	if err != nil {
+		fmt.Print("invalid regex4: %w", err)
+	}
+
+	err = SearchFileByLine(ls, "F:/go/dataclean/main/Source.txt")
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
+
+	sum := ls.Len()
+	questionclass := make(map[string]int)
+	questionanswer := make(map[string]int)
+	oneanswer := make(map[string]int)
+
+	yesorno := make(map[string]int)
+
+	//var q *Question
 	for e := ls.Front(); e != nil; e = e.Next() {
-		log.Printf("%v ", e.Value.(*Question).ToString())
+		if q, ok := e.Value.(*Question); ok {
+			q.Head = strings.ReplaceAll(q.Head, q.Answer, "")
+			//log.Printf("%v ", q.ToString())
+
+			//if q.Class == "未知" && q.Answer == "" {
+			//	log.Printf("%v ", e.Value.(*Question).ToString())
+			//}
+			questionanswer[q.Answer] = questionanswer[q.Answer] + 1
+			questionclass[q.Class] = questionclass[q.Class] + 1
+		}
+	}
+	oneanswer["A"] = questionanswer["A"]
+	oneanswer["B"] = questionanswer["B"]
+	oneanswer["C"] = questionanswer["C"]
+	oneanswer["D"] = questionanswer["D"]
+
+	yesorno["对"] = questionanswer["对"]
+	yesorno["错"] = questionanswer["错"]
+
+	delete(questionanswer, "A")
+	delete(questionanswer, "B")
+	delete(questionanswer, "C")
+	delete(questionanswer, "D")
+	delete(questionanswer, "对")
+	delete(questionanswer, "错")
+
+	s1, sum1 := sortMap(oneanswer)
+	s2, sum2 := sortMap(questionanswer)
+	s3, sum3 := sortMap(yesorno)
+
+	fmt.Printf("sum:%d;\n", sum)
+
+	fmt.Printf("题型:%v;\n", questionclass)
+
+	//fmt.Printf("单选答案:%v;\n", oneanswer)
+
+	for _, item := range *s1 {
+		fmt.Printf("单选题答案其中:%s: %d 个; 概率 : %.2f\n", item.Key, item.Value, float32(item.Value)/float32(sum1))
+	}
+
+	//fmt.Printf("多选答案:%v;\n", questionanswer)
+
+	for _, item := range *s2 {
+		fmt.Printf("多选题答案其中:%s: %d 个; 概率 : %.2f\n", item.Key, item.Value, float32(item.Value)/float32(sum2))
+	}
+
+	//fmt.Printf("判断答案:%v;\n", yesorno)
+
+	for _, item := range *s3 {
+		fmt.Printf("判断题答案其中:%s: %d 个; 概率 : %.2f\n", item.Key, item.Value, float32(item.Value)/float32(sum3))
+	}
+
+	for e := ls.Front(); e != nil; e = e.Next() {
+		if q, ok := e.Value.(*Question); ok {
+			if q.Answer != "ABCD" && q.Class == "多选题" {
+				fmt.Printf(q.ToString())
+			}
+			if q.Answer != "C" && q.Class == "单选题" {
+				fmt.Printf(q.ToString())
+			}
+			if q.Answer == "错" && q.Class == "判断题" {
+				fmt.Printf(q.ToString())
+			}
+		}
 	}
 }
